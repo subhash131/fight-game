7bf213032a6f4008cbe2a9469bb8781847d51df7
subhash131 <subhashnayak131@gmail.com>
2024-11-01 16:57:39 +0530

    initial frontend setup

--- frontend/js/eventListeners.js
+++ frontend/js/eventListeners.js
@@ -1,40 +1,48 @@
-window.addEventListener('keydown', (event) => {
+window.addEventListener("keydown", (event) => {
   switch (event.key) {
-    case 'w':
-      keys.w.pressed = true
-      break
-    case 'a':
-      keys.a.pressed = true
-      break
-    case 's':
-      keys.s.pressed = true
-      break
-    case 'd':
-      keys.d.pressed = true
-      break
+    case "w":
+    case "ArrowUp":
+      keys.w.pressed = true;
+      break;
+    case "a":
+    case "ArrowLeft":
+      keys.a.pressed = true;
+      break;
+    case "s":
+    case "ArrowDown":
+      keys.s.pressed = true;
+      break;
+    case "d":
+    case "ArrowRight":
+      keys.d.pressed = true;
+      break;
   }
-})
+});
 
-window.addEventListener('keyup', (event) => {
+window.addEventListener("keyup", (event) => {
   switch (event.key) {
-    case 'w':
-      keys.w.pressed = false
-      break
-    case 'a':
-      keys.a.pressed = false
-      break
-    case 's':
-      keys.s.pressed = false
-      break
-    case 'd':
-      keys.d.pressed = false
-      break
+    case "w":
+    case "ArrowUp":
+      keys.w.pressed = false;
+      break;
+    case "a":
+    case "ArrowLeft":
+      keys.a.pressed = false;
+      break;
+    case "s":
+    case "ArrowDown":
+      keys.s.pressed = false;
+      break;
+    case "d":
+    case "ArrowRight":
+      keys.d.pressed = false;
+      break;
   }
-})
+});
 
 // On return to game's tab, ensure delta time is reset
-document.addEventListener('visibilitychange', () => {
+document.addEventListener("visibilitychange", () => {
   if (!document.hidden) {
-    lastTime = performance.now()
+    lastTime = performance.now();
   }
-})
+});
